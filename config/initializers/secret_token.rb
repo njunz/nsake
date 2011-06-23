6f39e9c3d2a11fc40d77ec7d6d738237b7f64faf
njunz <njunichiro@gmail.com>
2011-06-23 23:47:29 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nsake::Application.config.secret_token = 'd51682f885cd34a8ffa46ae858118e5c597fdb65f1bb465385b1b39a57bdda0c55ecfae87ac05ba7fd22ea7e561c3bd3738a12e96e087dfcedf29e2e14926295'
